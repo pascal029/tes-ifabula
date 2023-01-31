ef7640a27bf326ed05d5f9143edd41df0f062c4f
pascal029 <pascalvirgiawan05@gmail.com>
2023-01-31 16:56:37 +0700

    soal utama point 5

--- soal-utama/src/pages/Tiga.js
+++ soal-utama/src/pages/Tiga.js
@@ -2,18 +2,23 @@ import { useState, useEffect } from "react";
 
 export default function Tiga() {
   const [data, setData] = useState([]);
+  const result = async () => {
+    const data = await fetch("http://jsonplaceholder.typicode.com/posts");
+    let json = await data.json();
+    console.log(json);
+    json = await json.filter((el) => el.id <= 10);
+    await setData(json);
+    return json;
+  };
   useEffect(() => {
-    const result = async () => {
-      const data = await fetch("http://jsonplaceholder.typicode.com/posts");
-      let json = await data.json();
-      console.log(json);
-      json = await json.filter((el) => el.id <= 10);
-      await setData(json);
-      return json;
-    };
-
     result();
   }, []);
+
+  const deleteFunction = (e, id) => {
+    e.preventDefault();
+    const filtered = data.filter((el) => el.id != id);
+    setData(filtered);
+  };
   return (
     <div>
       <table style={{ border: 1 }}>
@@ -33,6 +38,11 @@ export default function Tiga() {
                 <td>{el.id}</td>
                 <td>{el.title}</td>
                 <td>{el.body}</td>
+                <td>
+                  <button onClick={(e) => deleteFunction(e, el.id)}>
+                    DELETE
+                  </button>
+                </td>
               </tr>
             );
           })}
